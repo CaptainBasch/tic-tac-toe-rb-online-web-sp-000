10a191734456d4cb2661f47325e9ceb243d35774
Bailey Davis <bailey.gage.davis@gmail.com>
2020-05-28 15:06:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,27 +8,19 @@ def display_board(board)
 end
 
 
-
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
 
-
-
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
-
-
 
 
 def turn(board)
@@ -36,7 +28,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
